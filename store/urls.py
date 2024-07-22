eb9ddf0a9b76f0dc7aef35e1afcb2abe1bd2bc8b
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-23 01:01:31 +0300

    update on the customer, category, product, and order models

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
+
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
