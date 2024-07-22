27b07623b38441926ca00d9d69eecf928c680794
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-23 02:19:10 +0300

    modified home.html

--- store/models.py
+++ store/models.py
@@ -8,6 +8,9 @@ class Category (models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 # customers
 class Customer (models.Model):
 
