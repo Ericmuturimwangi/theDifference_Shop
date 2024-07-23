c55283f496750dc84fd22065b4b333a11b40c336
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-24 01:01:21 +0300

    modified authentication of the website

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
