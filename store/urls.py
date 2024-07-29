98145f1b8f09dff3c77d88c5e9783f1f9180b1f2
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-29 23:08:46 +0300

    modified the user profile, user can change their profile except password field

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", views.home, name='home'),
     path('about/', views.about, name='about'),
     path('login/', views.login_user, name='login'),
+    path('update_user/', views.update_user, name='update_user'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('product/<int:pk>', views.product, name='product'),
