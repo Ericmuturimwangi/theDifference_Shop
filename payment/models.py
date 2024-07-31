49bdbdba38700c28614d5ee6eb50aa366327479e
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-31 22:08:03 +0300

    created the shipping address for a new user

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -15,13 +16,23 @@ class ShippingAddress(models.Model):
     shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
     shipping_country = models.CharField(max_length=255)
 
-
-# avoid plurals
+    # avoid plurals
     class Meta:
         verbose_name_plural = "Shipping Address"
 
     def __Str__(self):
-        return f'Shipping Address - {str(self.id)}'
+        return f"Shipping Address - {str(self.id)}"
+
+
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+
+# automate the profile
+post_save.connect(create_shipping, sender=User)
+
 
 # order model
 class Order(models.Model):
@@ -34,7 +45,8 @@ class Order(models.Model):
     date_ordered = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f'Order - {str(self.id)}'
+        return f"Order - {str(self.id)}"
+
 
 # order items model
 class OrderItem(models.Model):
@@ -46,5 +58,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
-        return f'Order Item - {str(self.id)}'
-        
+        return f"Order Item - {str(self.id)}"
