001d6cc37e5ea82ea31018856a9a15f1fa1bb169
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-07-31 23:34:38 +0300

    created the billing section

--- payment/urls.py
+++ payment/urls.py
@@ -1,9 +1,8 @@
-
-from .import views
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("payment_success", views.payment_success, name='payment_success'),
-    path("checkout", views.checkout, name='checkout'),
-    
-] 
+    path("payment_success", views.payment_success, name="payment_success"),
+    path("checkout", views.checkout, name="checkout"),
+    path("billing_info", views.billing_info, name="billing_info"),
+]
--- payment/views.py
+++ payment/views.py
@@ -1,12 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages
 
 
-# Create your views here.
+def billing_info(request):
+
+    if request.POST:
+        # get the billing_form
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # check to see if user is logged in
+
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+
+        else:
+            billing_form = PaymentForm()
+            return render(
+                request,
+                "payment/billing_info.html",
+                {
+                    "cart_products": cart_products,
+                    "quantities": quantities,
+                    "totals": totals,
+                    "shipping_info": request.POST,
+                    "billing_form": billing_form,
+                },
+            )
+        shipping_form = request.POST
+        return render(
+            request,
+            "payment/billing_info.html",
+            {
+                "cart_products": cart_products,
+                "quantities": quantities,
+                "totals": totals,
+                "shipping_form": shipping_form,
+            },
+        )
+    else:
+        messages.success(request, "Access Denied!")
+        return redirect("home")
+
+
 def payment_success(request):
-
     return render(request, "payment/payment_success.html", {})
 
 
@@ -17,8 +72,12 @@ def checkout(request):
     totals = cart.cart_total()
 
     if request.user.is_authenticated:
-        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
-        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        try:
+            shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+            shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        except ShippingAddress.DoesNotExist:
+            # Handle the case where the shipping address does not exist
+            shipping_form = ShippingForm(request.POST or None)
         return render(
             request,
             "payment/checkout.html",
