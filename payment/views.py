1421d3de3587ca6c01ce200cbb28eb8fc48cc50e
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-08-01 11:44:23 +0300

    created the shipped and unshipped buttomn to appear on both pages as an update

--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from django.contrib.auth.models import User
 from store.models import Product
+import datetime
 
 
 def orders(request, pk):
@@ -15,6 +16,21 @@ def orders(request, pk):
         order = Order.objects.get(id=pk)
         # get the order items
         items = OrderItem.objects.filter(order=pk)
+
+        if request.POST:
+            status = request.POST["shipping_status"]
+            # check if true or false
+            if status == "true":
+                # get the order
+                order = Order.objects.filter(id=pk)
+                # update status
+                now = datetime.datetime.now()
+                order.update(shipped=True)
+            else:
+                order = Order.objects.filter(id=pk)
+                order.update(shipped=False)
+            messages.success(request, "Shipping Status Updated")
+            return redirect("home")
 
         return render(request, "payment/orders.html", {"order": order, "items": items})
     else:
@@ -26,6 +42,17 @@ def not_shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
 
         orders = Order.objects.filter(shipped=False)
+        if request.POST:
+            status = request.POST["shipping_status"]
+            num = request.POST["num"]
+            order = Order.objects.filter(id=num)
+            # grab date time
+            now = datetime.datetime.now()
+            # update order
+            order.update(shipped=True, date_shipped=now)
+            # redirect
+            messages.success(request, "Shipping Status Updated")
+            return redirect("home")
 
         return render(
             request,
@@ -40,6 +67,18 @@ def not_shipped_dash(request):
 def shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
         orders = Order.objects.filter(shipped=True)
+        if request.POST:
+            status = request.POST["shipping_status"]
+            num = request.POST["num"]
+            order = Order.objects.filter(id=num)
+            # grab date time
+            now = datetime.datetime.now()
+            # update order
+            order.update(shipped=False)
+            # redirect
+            messages.success(request, "Shipping Status Updated")
+            return redirect("home")
+
         return render(
             request,
             "payment/shipped_dash.html",
