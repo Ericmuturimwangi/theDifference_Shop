c296d87d5f870acd7ba257199cfddf7177a304ea
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-08-01 15:55:50 +0300

    modified the DB_PASSWORD_YO

--- ecom/settings.py
+++ ecom/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 import os
 
-print(os.getenv("DB_PASSWORD_YO"))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
